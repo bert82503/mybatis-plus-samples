097115500de509d5dd335e8bf0289b34856e191d
bert82503 <bert825_work@163.com>
2023-02-25 21:43:27 +0800

    learn MybatisPlusInterceptor,PaginationInnerInterceptor

--- mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/config/MybatisPlusConfig.java
@@ -7,26 +7,31 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+/**
+ * @author hubin
+ * @see com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
+ * @see com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor
+ */
 @Configuration
 public class MybatisPlusConfig {
-	/**
-     *	 分页插件
+    /**
+     * 分页插件
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        // 拦截器
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页查询拦截器
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
 
-   
-
     /**
-     *	 注入主键生成器
+     * 注入主键生成器
      */
     @Bean
-    public IKeyGenerator keyGenerator(){
+    public IKeyGenerator keyGenerator() {
+        // 主键生成器
         return new H2KeyGenerator();
-    }    
+    }
 }
