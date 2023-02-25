1fcf8e64d8dc1ba7500aaae10c09a1d5a11c50be
bert82503 <bert825_work@163.com>
2023-02-25 20:59:14 +0800

    learn QueryWrapper<T>,UpdateWrapper<T>

--- mybatis-plus-sample-active-record/src/test/java/com/baomidou/mybatisplus/samples/ar/ActiveRecordTest.java
+++ mybatis-plus-sample-active-record/src/test/java/com/baomidou/mybatisplus/samples/ar/ActiveRecordTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
  *
  * @author hubin
  * @since 2018-08-11
+ * @see com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
+ * @see com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper
  */
 @SpringBootTest
 public class ActiveRecordTest {
@@ -24,23 +26,27 @@ public class ActiveRecordTest {
         user.setName("咩咩");
         user.setAge(5);
         user.setEmail("miemie@mp.com");
+        // 插入
         Assertions.assertTrue(user.insert());
         // 成功可以直接获取 ID
-        System.err.println("\n插入成功 ID 为：" + user.getId());
+        System.out.println("\n插入成功 ID 为：" + user.getId());
     }
 
 
     @Test
     public void bDelete() {
+        // 删除
         Assertions.assertTrue(new User().setId(3L).deleteById());
-        Assertions.assertTrue(new User().delete(new QueryWrapper<User>()
-                .lambda().eq(User::getName, "Sandy")));
+        // 查询条件
+        Assertions.assertTrue(new User().delete(new QueryWrapper<User>().lambda()
+                .eq(User::getName, "Sandy")));
     }
 
 
     @Test
     public void cUpdate() {
         Assertions.assertTrue(new User().setId(1L).setEmail("ab@c.c").updateById());
+        // 更新条件和语句
         Assertions.assertTrue(new User().update(new UpdateWrapper<User>().lambda()
                         .set(User::getAge, 3).eq(User::getId, 2)));
     }
@@ -49,8 +55,10 @@ public class ActiveRecordTest {
     @Test
     public void dSelect() {
         Assertions.assertEquals("ab@c.c", new User().setId(1L).selectById().getEmail());
-        User user = new User().selectOne(new QueryWrapper<User>().lambda().eq(User::getId, 2));
+        // 查询条件
+        User user = new User().selectOne(new QueryWrapper<User>().lambda()
+                .eq(User::getId, 2));
         Assertions.assertEquals("Jack", user.getName());
-        Assertions.assertTrue(3 == user.getAge());
+        Assertions.assertEquals(user.getAge(), 3);
     }
 }
