e2d62bee330ef516065a75b252816a61ea38af0f
bert82503 <bert825_work@163.com>
2023-02-25 21:39:49 +0800

    learn @TableName

--- mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/entity/Company.java
+++ mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/entity/Company.java
@@ -5,11 +5,14 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * @author hubin
+ * @see com.baomidou.mybatisplus.annotation.TableName
+ */
 @Data
 @TableName("company")
 public class Company {
 	private Long id;
     private String name;
     List<User> userList;
-
 }
