90e7dc79331fc4c55061047abcda430b42436186
bert82503 <bert825_work@163.com>
2023-02-25 21:45:44 +0800

    learn IPage<T>

--- mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/mapper/UserMapper.java
+++ mybatis-plus-sample-association/src/main/java/com/baomidou/mybatisplus/samples/association/mapper/UserMapper.java
@@ -9,8 +9,19 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+/**
+ * @author hubin
+ * @see com.baomidou.mybatisplus.core.metadata.IPage
+ */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
+    /**
+     * 分页查询用户列表
+     *
+     * @param page 分页参数
+     * @param wrapper 查询条件
+     * @return 用户列表
+     */
     List<User> selectUserPage(IPage<User> page, @Param("ew") QueryWrapper<User> wrapper);
 }
