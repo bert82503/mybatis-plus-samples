14fe985be2fb0d247c3045baf4ae5515c65684d8
bert82503 <bert825_work@163.com>
2023-02-25 20:36:24 +0800

    learn @MapperScan

--- mybatis-plus-sample-active-record/src/main/java/com/baomidou/mybatisplus/samples/ar/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-active-record/src/main/java/com/baomidou/mybatisplus/samples/ar/config/MybatisPlusConfig.java
@@ -4,11 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 自动配置
+ *
  * @author miemie
  * @since 2018-08-10
+ * @see org.mybatis.spring.annotation.MapperScan
  */
 @Configuration
 @MapperScan("com.baomidou.mybatisplus.samples.ar.mapper")
 public class MybatisPlusConfig {
-
+    //
 }
