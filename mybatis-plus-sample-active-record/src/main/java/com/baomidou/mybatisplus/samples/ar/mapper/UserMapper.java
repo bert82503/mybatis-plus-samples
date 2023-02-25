dd9a231e54eff569f2858956274ab1c258c6de52
bert82503 <bert825_work@163.com>
2023-02-25 20:34:44 +0800

    learn BaseMapper<T> extends Mapper<T>

--- mybatis-plus-sample-active-record/src/main/java/com/baomidou/mybatisplus/samples/ar/mapper/UserMapper.java
+++ mybatis-plus-sample-active-record/src/main/java/com/baomidou/mybatisplus/samples/ar/mapper/UserMapper.java
@@ -5,15 +5,16 @@ import com.baomidou.mybatisplus.samples.ar.entity.User;
 
 /**
  * <p>
- * 这个得有，就算不去用它否则默认不注入
+ * 这个得有，就算不去用它；否则，默认不注入。
  * </p>
  * <p>
- * MP 支持不需要 UserMapper.xml 这个模块演示内置 CRUD 咱们就不要 XML 部分了
+ * MP 支持不需要 UserMapper.xml ，这个模块演示内置 CRUD ，咱们就不要 XML 部分了
  * </p>
  *
  * @author hubin
  * @since 2018-08-11
+ * @see com.baomidou.mybatisplus.core.mapper.BaseMapper
  */
 public interface UserMapper extends BaseMapper<User> {
-
+    //
 }
