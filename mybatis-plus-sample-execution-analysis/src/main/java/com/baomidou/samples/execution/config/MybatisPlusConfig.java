53733110ec597d5d23418acfee88f716eeb63f67
bert82503 <bert825_work@163.com>
2023-03-16 19:40:15 +0800

    learn BlockAttackInnerInterceptor

--- mybatis-plus-sample-execution-analysis/src/main/java/com/baomidou/samples/execution/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-execution-analysis/src/main/java/com/baomidou/samples/execution/config/MybatisPlusConfig.java
@@ -8,14 +8,17 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MybatisPlus配置文件
  *
- * @author nieqiurong 2018/8/11 21:10.
+ * @author nieqiurong 2018/8/11 21:10
+ * @see com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor
  */
 @Configuration
 public class MybatisPlusConfig {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        // 拦截器
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 攻击 SQL 阻断解析器，防止全表更新与删除
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
         return interceptor;
     }
