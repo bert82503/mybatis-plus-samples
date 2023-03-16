0ef280a1134de4d8f0c62d990952afc90e554340
bert82503 <bert825_work@163.com>
2023-03-16 19:19:57 +0800

    learn DynamicTableNameInnerInterceptor,TableNameHandler

--- mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/DynamicTableNameApplication.java
+++ mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/DynamicTableNameApplication.java
@@ -3,6 +3,9 @@ package com.baomidou.mybatisplus.samples.dytablename;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 动态表名的应用程序入口。
+ */
 @SpringBootApplication
 public class DynamicTableNameApplication {
 
--- mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
+++ mybatis-plus-sample-dynamic-tablename/src/main/java/com/baomidou/mybatisplus/samples/dytablename/config/MybatisPlusConfig.java
@@ -10,8 +10,13 @@ import java.util.Map;
 import java.util.Random;
 
 /**
+ * 动态表名的自动配置。
+ *
  * @author miemie
  * @since 2018-08-10
+ * @see com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor
+ * @see com.baomidou.mybatisplus.extension.plugins.handler.TableNameHandler
+ * @see com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
  */
 @Configuration
 @MapperScan("com.baomidou.mybatisplus.samples.dytablename.mapper")
@@ -19,8 +24,11 @@ public class MybatisPlusConfig {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        // 拦截器
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 动态表名的拦截器
         DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
+        // 表名处理器
         dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
             // 获取参数方法
             Map<String, Object> paramMap = RequestDataHelper.getRequestData();
