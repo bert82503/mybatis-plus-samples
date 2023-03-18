29564105d6dc3044112aae41575cd52af7a2ef93
bert82503 <bert825_work@163.com>
2023-03-19 01:04:53 +0800

    learn @TableId,@TableField,@Version

--- mybatis-plus-sample-mysql/src/main/java/com/baomidou/mybatisplus/samples/mysql/entity/TestData.java
+++ mybatis-plus-sample-mysql/src/main/java/com/baomidou/mybatisplus/samples/mysql/entity/TestData.java
@@ -15,27 +15,36 @@ import java.time.LocalDateTime;
  * 2.乐观锁          验证无误
  * 3.多租户          验证无误
  * 4.枚举            验证无误
+ * @see TableId
+ * @see TableField
+ * @see Version
  */
 @Data
 @Accessors(chain = true)
 public class TestData implements Serializable {
+
+    private static final long serialVersionUID = -2834123400311477879L;
+
     @TableId(type = IdType.AUTO)
     private Long id;
     @TableField(jdbcType = JdbcType.INTEGER)
     private Integer testInt;
     @TableField("test_str")
     private String testStr;
+
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
     @TableField(fill = FieldFill.UPDATE)
     private LocalDateTime updateTime;
+
     private TestEnum testEnum;
 
     @Version
     private Integer version;
+
     /**
      * 多租户
-     * 不用配置实体字段,但是数据库需要该字段
+     * 不用配置实体字段，但是数据库需要该字段
      */
     private Long tenantId;
 
