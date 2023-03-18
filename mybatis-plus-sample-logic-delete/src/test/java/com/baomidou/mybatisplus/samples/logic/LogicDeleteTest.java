68cf0f6d4a996d358d4d7551dd4666792195a2b8
bert82503 <bert825_work@163.com>
2023-03-19 00:56:47 +0800

    learn @SpringBootTest,@Test,@TestMethodOrder

--- mybatis-plus-sample-logic-delete/src/test/java/com/baomidou/mybatisplus/samples/logic/LogicDeleteTest.java
+++ mybatis-plus-sample-logic-delete/src/test/java/com/baomidou/mybatisplus/samples/logic/LogicDeleteTest.java
@@ -24,6 +24,7 @@ import java.util.List;
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 @SpringBootTest
 public class LogicDeleteTest {
+
     @Resource
     private CommonMapper commonMapper;
     @Resource
