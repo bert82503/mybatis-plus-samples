6443e89c3752cae8084021b61e325a670acb2593
bert82503 <bert825_work@163.com>
2023-03-19 00:30:09 +0800

    learn BaseMapper<T> extends Mapper<T>

--- mybatis-plus-sample-id-string/src/main/java/com/baomidou/samples/mapper/UserMapper.java
+++ mybatis-plus-sample-id-string/src/main/java/com/baomidou/samples/mapper/UserMapper.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.samples.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * @see com.baomidou.mybatisplus.core.mapper.BaseMapper
+ */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-
+    //
 }
