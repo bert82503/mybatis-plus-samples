30cbfb1835e634374cd94ea42c0a83765a254ddb
bert82503 <bert825_work@163.com>
2023-03-19 23:18:50 +0800

    learn PaginationTest

--- mybatis-plus-sample-pagination/src/test/java/com/baomidou/mybatisplus/samples/pagination/PaginationTest.java
+++ mybatis-plus-sample-pagination/src/test/java/com/baomidou/mybatisplus/samples/pagination/PaginationTest.java
@@ -11,7 +11,6 @@ import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.CollectionUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
@@ -39,12 +38,13 @@ import lombok.extern.slf4j.Slf4j;
 class PaginationTest {
 
     @Resource
-    private UserMapper mapper;
+    private UserMapper userMapper;
 
     @Test
     void lambdaPagination() {
         Page<User> page = new Page<>(1, 3);
-        Page<User> result = mapper.selectPage(page, Wrappers.<User>lambdaQuery().ge(User::getAge, 1).orderByAsc(User::getAge));
+        Page<User> result = userMapper.selectPage(page, Wrappers.<User>lambdaQuery()
+                .ge(User::getAge, 1).orderByAsc(User::getAge));
         assertThat(result.getTotal()).isGreaterThan(3);
         assertThat(result.getRecords().size()).isEqualTo(3);
     }
@@ -54,7 +54,8 @@ class PaginationTest {
         log.error("----------------------------------baseMapper 自带分页-------------------------------------------------------");
         Page<User> page = new Page<>(1, 5);
         page.addOrder(OrderItem.asc("age"));
-        Page<User> userIPage = mapper.selectPage(page, Wrappers.<User>lambdaQuery().eq(User::getAge, 20).like(User::getName, "Jack"));
+        Page<User> userIPage = userMapper.selectPage(page, Wrappers.<User>lambdaQuery()
+                .eq(User::getAge, 20).like(User::getName, "Jack"));
         assertThat(page).isSameAs(userIPage);
         log.error("总条数 -------------> {}", userIPage.getTotal());
         log.error("当前页数 -------------> {}", userIPage.getCurrent());
@@ -74,7 +75,7 @@ class PaginationTest {
         log.error("----------------------------------自定义 XML 分页-------------------------------------------------------");
         MyPage<User> myPage = new MyPage<User>(1, 5).setSelectInt(20).setSelectStr("Jack");
         ParamSome paramSome = new ParamSome(20, "Jack");
-        MyPage<User> userMyPage = mapper.mySelectPage(myPage, paramSome);
+        MyPage<User> userMyPage = userMapper.mySelectPage(myPage, paramSome);
         assertThat(myPage).isSameAs(userMyPage);
         log.error("总条数 -------------> {}", userMyPage.getTotal());
         log.error("当前页数 -------------> {}", userMyPage.getCurrent());
@@ -86,46 +87,41 @@ class PaginationTest {
         /* 下面的 left join 不会对 count 进行优化,因为 where 条件里有 join 的表的条件 */
         MyPage<UserChildren> myPage = new MyPage<>(1, 5);
         myPage.setSelectInt(18).setSelectStr("Jack");
-        MyPage<UserChildren> userChildrenMyPage = mapper.userChildrenPage(myPage);
+        MyPage<UserChildren> userChildrenMyPage = userMapper.userChildrenPage(myPage);
         List<UserChildren> records = userChildrenMyPage.getRecords();
         records.forEach(System.out::println);
 
         /* 下面的 left join 会对 count 进行优化,因为 where 条件里没有 join 的表的条件 */
         myPage = new MyPage<UserChildren>(1, 5).setSelectInt(18);
-        userChildrenMyPage = mapper.userChildrenPage(myPage);
+        userChildrenMyPage = userMapper.userChildrenPage(myPage);
         records = userChildrenMyPage.getRecords();
         records.forEach(System.out::println);
     }
 
-    private <T> void print(List<T> list) {
-        if (!CollectionUtils.isEmpty(list)) {
-            list.forEach(System.out::println);
-        }
-    }
-
-
     @Test
     void testMyPageMap() {
         MyPage<User> myPage = new MyPage<User>(1, 5).setSelectInt(20).setSelectStr("Jack");
-        mapper.mySelectPageMap(myPage, Maps.newHashMap("name", "%a"));
+        userMapper.mySelectPageMap(myPage, Maps.newHashMap("name", "%a"));
         myPage.getRecords().forEach(System.out::println);
     }
 
     @Test
     void testMap() {
-        mapper.mySelectMap(Maps.newHashMap("name", "%a")).forEach(System.out::println);
+        userMapper.mySelectMap(Maps.newHashMap("name", "%a")).forEach(System.out::println);
     }
 
     @Test
     void myPage() {
         MyPage<User> page = new MyPage<>(1, 5);
         page.setName("a");
-        mapper.myPageSelect(page).forEach(System.out::println);
+        userMapper.myPageSelect(page).forEach(System.out::println);
     }
 
     @Test
     void iPageTest() {
         IPage<User> page = new Page<User>(1, 5) {
+            private static final long serialVersionUID = -7005156063022048004L;
+
             private String name = "%";
 
             public String getName() {
@@ -137,7 +133,7 @@ class PaginationTest {
             }
         };
 
-        List<User> list = mapper.iPageSelect(page);
+        List<User> list = userMapper.iPageSelect(page);
         System.out.println("list.size=" + list.size());
         System.out.println("page.total=" + page.getTotal());
     }
@@ -150,7 +146,8 @@ class PaginationTest {
         //使用三参数的构造器创建Page对象
         //第三个参数isSearchCount：传true则查询总记录数;传false则不查询总记录数（既不进行count查询）
         Page<User> page = new Page<>(1,3,false);
-        Page<User> result = mapper.selectPage(page, Wrappers.<User>lambdaQuery().ge(User::getAge, 20));
+        Page<User> result = userMapper.selectPage(page, Wrappers.<User>lambdaQuery()
+                .ge(User::getAge, 20));
         assertThat(result.getRecords().size()).isEqualTo(3);
         //因为没有进行count查询，total值为0
         assertThat(result.getTotal()).isEqualTo(0);
@@ -159,7 +156,7 @@ class PaginationTest {
     @Test
     void rowBoundsTest() {
         RowBounds rowBounds = new RowBounds(0, 5);
-        List<User> list = mapper.rowBoundList(rowBounds, Maps.newHashMap("name", "%"));
+        List<User> list = userMapper.rowBoundList(rowBounds, Maps.newHashMap("name", "%"));
         System.out.println("list.size=" + list.size());
     }
 
@@ -167,26 +164,30 @@ class PaginationTest {
     void selectAndGroupBy() {
         LambdaQueryWrapper<User> lq = new LambdaQueryWrapper<>();
         lq.select(User::getAge).groupBy(User::getAge);
-        for (User user : mapper.selectList(lq)) {
+        for (User user : userMapper.selectList(lq)) {
             System.out.println(user.getAge());
         }
     }
 
     @Autowired
-    IUserService userService;
+    private IUserService userService;
 
     @Test
     void lambdaPageTest() {
         LambdaQueryChainWrapper<User> wrapper2 = userService.lambdaQuery();
         wrapper2.like(User::getName, "a");
-        userService.page(new Page<>(1, 10), wrapper2.getWrapper()).getRecords().forEach(System.out::print);
+        userService.page(new Page<>(1, 10), wrapper2.getWrapper())
+                .getRecords().forEach(System.out::print);
     }
 
     @Test
     void test() {
-        userService.lambdaQuery().like(User::getName, "a").list().forEach(System.out::println);
+        userService.lambdaQuery().like(User::getName, "a")
+                .list().forEach(System.out::println);
 
-        Page page = userService.lambdaQuery().like(User::getName, "a").page(new Page<>(1, 10));
+        Page<User> page = userService.lambdaQuery()
+                .like(User::getName, "a")
+                .page(new Page<>(1, 10));
         page.getRecords().forEach(System.out::println);
     }
 }
