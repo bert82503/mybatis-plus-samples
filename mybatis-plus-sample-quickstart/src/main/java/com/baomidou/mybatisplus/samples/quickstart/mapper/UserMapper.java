42b4de6d717f2281aed5fdca0a521448bb1699f2
bert82503 <bert825_work@163.com>
2023-03-19 23:32:55 +0800

    learn BaseMapper<T>

--- mybatis-plus-sample-quickstart/src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
+++ mybatis-plus-sample-quickstart/src/main/java/com/baomidou/mybatisplus/samples/quickstart/mapper/UserMapper.java
@@ -3,6 +3,9 @@ package com.baomidou.mybatisplus.samples.quickstart.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.samples.quickstart.entity.User;
 
+/**
+ * @see com.baomidou.mybatisplus.core.mapper.BaseMapper
+ */
 public interface UserMapper extends BaseMapper<User> {
-
+    //
 }
