b2c2ab20798b54e17014f55c5d0e70019ce2e12f
bert82503 <bert825_work@163.com>
2023-03-19 22:55:37 +0800

    learn BaseMapper<T>

--- mybatis-plus-sample-pagehelper/src/main/java/com/baomidou/mybatisplus/samples/pagehelper/UserMapper.java
+++ mybatis-plus-sample-pagehelper/src/main/java/com/baomidou/mybatisplus/samples/pagehelper/UserMapper.java
@@ -7,8 +7,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
  * @author miemie
  * @since 2020-05-29
+ * @see com.baomidou.mybatisplus.core.mapper.BaseMapper
  */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-
+    //
 }
