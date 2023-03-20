c6a8819fa61ac918eb8ea8beaf1dd46484c7396e
bert82503 <bert825_work@163.com>
2023-03-20 23:55:36 +0800

    learn DefaultSqlInjector extends AbstractSqlInjector

--- mybatis-plus-sample-sql-injector/src/main/java/com/baomidou/samples/injector/base/MySqlInjector.java
+++ mybatis-plus-sample-sql-injector/src/main/java/com/baomidou/samples/injector/base/MySqlInjector.java
@@ -15,17 +15,19 @@ import java.util.List;
 /**
  * 自定义Sql注入
  *
- * @author nieqiurong 2018/8/11 20:23.
+ * @author nieqiurong 2018/8/11 20:23
+ * @see com.baomidou.mybatisplus.core.injector.DefaultSqlInjector
  */
 public class MySqlInjector extends DefaultSqlInjector {
 
     @Override
     public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        // 方法列表
         List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
-        //增加自定义方法
+        // 增加自定义方法
         methodList.add(new DeleteAll());
         methodList.add(new FindOne());
-        /**
+        /*
          * 以下 3 个为内置选装件
          * 头 2 个支持字段筛选函数
          */
